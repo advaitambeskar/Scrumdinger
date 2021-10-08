4524e9f0e66e228b8731cad1b25201ce9f6b83d5
Advait Ambeskar <ambeskaradvait@gmail.com>
2021-10-09 03:53:31 +0530

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Advait Ambeskar on 09/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
