285d8c2951984225f994b64f87fb34eea7c571aa
Advait Ambeskar <ambeskaradvait@gmail.com>
2021-10-09 06:07:07 +0530

    Added edit - until chapter 4

--- Scrumdinger/DetailView.swift
+++ Scrumdinger/DetailView.swift
@@ -9,6 +9,7 @@ import Foundation
 import SwiftUI
 
 struct DetailView: View {
+    @State private var isPresented = false
     let scrum: DailyScrum
     var body: some View {
         List {
@@ -42,7 +43,21 @@ struct DetailView: View {
             }
         }
         .listStyle(InsetGroupedListStyle())
+        .navigationBarItems(trailing: Button("Edit"){
+            isPresented = true
+        })
         .navigationBarTitle(scrum.title)
+        .fullScreenCover(isPresented: $isPresented){
+            NavigationView(){
+                EditView()
+                    .navigationTitle(scrum.title)
+                    .navigationBarItems(leading: Button("Cancel"){
+                        isPresented = false
+                    }, trailing: Button("Done"){
+                        isPresented = false
+                    })
+            }
+        }
     }
 }
 
